e6286468a20f84c185d1ee81dfd8bf0309d47b47
omerUnver <muhammedomerunver@gmail.com>
2022-08-06 09:36:41 +0300

    Initial Commit

---
+++ Anka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anka
+//
+//  Created by M.Ömer Ünver on 6.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AnkaUITests/AnkaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AnkaUITestsLaunchTests.swift
+//  AnkaUITests
+//
+//  Created by M.Ömer Ünver on 6.08.2022.
+//
+
+import XCTest
+
+class AnkaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
